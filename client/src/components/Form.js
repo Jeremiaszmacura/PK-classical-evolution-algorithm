c4a92ac480d425a7dcd3e9adc2385e1476086201
Konrad Makselan <makselan.konrad@gmail.com>
2022-04-03 14:28:33 +0200

    Forms extended and repaired

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -4,22 +4,32 @@ import doFetch from "../services/doFetch";
 
 const Form = () => {
 
-    const [numberOfPopulation, setNumberOfPopulation] = useState(0);
-    const [lengthOfChromosome, setLengthOfChromosome] = useState(0);
-    const [epochs, setEpochs] = useState(0);
-    const [crossProbability, setCrossProbability] = useState(0);
-    const [inversionProbability, setInversionProbability] = useState(0);
-    const [selectionBestPercent, setSelectionBestPercent] = useState(0);
-    const [elitistStrategyPercent, setElitistStrategyPercent] = useState(0);
+    const [numberOfPopulation, setNumberOfPopulation] = useState(0)
+    const [lengthOfChromosome, setLengthOfChromosome] = useState(0)
+    const [epochs, setEpochs] = useState(0)
+    const [crossProbability, setCrossProbability] = useState(0)
+    const [mutationProbability, setMutationProbability] = useState(0)
+    const [inversionProbability, setInversionProbability] = useState(0)
+    const [selectionPercent, setSelectionPercent] = useState(0)
+    const [elitistStrategyPercent, setElitistStrategyPercent] = useState(0)
+    const [sizeOfTournament, setSizeOfTournament] = useState(0)
+    const [selectionName, setSelectionName] = useState("best")
+    const [crossoverName, setCrossoverName] = useState("onePoint")
+    const [mutationName, setMutationName] = useState("edge")
 
     const parameters = {
         numberOfPopulation,
         lengthOfChromosome,
         epochs,
         crossProbability,
+        mutationProbability,
         inversionProbability,
-        selectionBestPercent,
-        elitistStrategyPercent
+        selectionPercent,
+        elitistStrategyPercent,
+        sizeOfTournament,
+        selectionName,
+        crossoverName,
+        mutationName
     }
 
     const send = (event) => {
@@ -62,53 +72,105 @@ const Form = () => {
                         className={style.formInput}
                         type='number'
                         value={epochs}
-                        min={0}
-                        max={100}
                         onChange={(e => setEpochs(e.target.value))}
                     />
-                </div> <div className={style.formInputs}>
+                </div>
+                <div className={style.formInputs}>
                     <label className={style.formLabel}>Cross probability</label>
                     <input
                         className={style.formInput}
                         type='number'
                         value={crossProbability}
-                        min={0}
-                        max={100}
+                        step={"0.01"}
                         onChange={(e => setCrossProbability(e.target.value))}
                     />
-                </div> <div className={style.formInputs}>
+                </div>
+                <div className={style.formInputs}>
+                    <label className={style.formLabel}>Mutation probability</label>
+                    <input
+                        className={style.formInput}
+                        type='number'
+                        value={mutationProbability}
+                        step={"0.01"}
+                        onChange={(e => setMutationProbability(e.target.value))}
+                    />
+                </div>
+                <div className={style.formInputs}>
                     <label className={style.formLabel}>Inversion Probability</label>
                     <input
                         className={style.formInput}
                         type='number'
                         value={inversionProbability}
-                        min={0}
-                        max={100}
+                        step={"0.01"}
                         onChange={(e => setInversionProbability(e.target.value))}
                     />
-                </div> <div className={style.formInputs}>
+                </div>
+                <div className={style.formInputs}>
                     <label className={style.formLabel}>Selection best percent</label>
                     <input
                         className={style.formInput}
                         type='number'
-                        value={selectionBestPercent}
-                        min={0}
-                        max={100}
-                        onChange={(e => setSelectionBestPercent(e.target.value))}
+                        value={selectionPercent}
+                        step={"0.01"}
+                        onChange={(e => setSelectionPercent(e.target.value))}
                     />
-                </div> <div className={style.formInputs}>
+                </div>
+                <div className={style.formInputs}>
                     <label className={style.formLabel}>Elitist Strategy Percent</label>
                     <input
                         className={style.formInput}
                         type='number'
                         value={elitistStrategyPercent}
-                        min={0}
-                        max={100}
+                        step={"0.01"}
                         onChange={(e => setElitistStrategyPercent(e.target.value))}
                     />
                 </div>
+                <div className={style.formInputs}>
+                    <label className={style.formLabel}>Size of Tournament</label>
+                    <input
+                        className={style.formInput}
+                        type='number'
+                        value={sizeOfTournament}
+                        max={numberOfPopulation}
+                        onChange={(e => setSizeOfTournament(e.target.value))}
+                    />
+                </div>
+                <div className={style.formInputs}>
+                    <label className={style.formLabel}>Selection Name</label>
+                    <select value={selectionName}
+                            onChange={(e => setSelectionName(e.target.value))}
+                            className={style.formInput}
+                    >
+                        <option value={"best"}>Best</option>
+                        <option value={"roulette"}>Roulette</option>
+                        <option value={"tournament"}>Tournament</option>
+                    </select>
+                </div>
+                <div className={style.formInputs}>
+                    <label className={style.formLabel}>Crossover Name</label>
+                    <select value={crossoverName}
+                            onChange={(e => setCrossoverName(e.target.value))}
+                            className={style.formInput}
+                    >
+                        <option value={"one_point"}>One point</option>
+                        <option value={"two_points"}>Two points</option>
+                        <option value={"three_points"}>Three points</option>
+                        <option value={"uniform"}>Uniform</option>
+                    </select>
+                </div>
+                <div className={style.formInputs}>
+                    <label className={style.formLabel}>Mutation Name</label>
+                    <select value={mutationName}
+                            onChange={(e => setMutationName(e.target.value))}
+                            className={style.formInput}
+                    >
+                        <option value={"edge"}>Edge</option>
+                        <option value={"one_point"}>One point</option>
+                        <option value={"two_points"}>Two points</option>
+                    </select>
+                </div>
                 <button type='submit'>
-                    Go
+                    Start
                 </button>
             </form>
         </div>
